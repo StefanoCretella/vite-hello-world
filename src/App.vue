9d5aa81b3b8d0d7ec93b3378863e866102039275
StefanoCretella <cretellastefano213@gmail.com>
2024-05-30 15:17:29 +0200

    [UPD] Esercizio svolto in ritardo a causa di problemi  nel farlo partire

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div id="app">
+    <AppTitle />
+    <p>Welcome to my Vite app</p>
+  </div>
+</template>
+
+<script>
+import AppTitle from './components/AppTitle.vue';
+
+export default {
+  components: {
+    AppTitle
+  }
+};
+</script>
